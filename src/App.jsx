2e95173c68a1931fcc7a46b6c59f6ad576c8c845
张宇昂 <zhangyuang@zhangyuangdeMacBook-Pro.local>
2024-01-03 17:55:40 +0800

    gp4

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SpinnerFullPage from './components/SpinnerFullPage';
 import { CityProvider } from './context/CityContext';
 import { AuthProvider } from './context/FakeAuthContext';
 import ProtectedRoute from './pages/ProtectedRoute';
+import ReactGA from 'react-ga';
 
 const Homepage = lazy(() => import('./pages/Homepage'));
 const Product = lazy(() => import('./pages/Product'));
@@ -16,6 +17,8 @@ const Pricing = lazy(() => import('./pages/Pricing'));
 const PageNotFound = lazy(() => import('./pages/PageNotFound'));
 const AppLayout = lazy(() => import('./pages/AppLayout'));
 const Login = lazy(() => import('./pages/Login'));
+
+ReactGA.initialize('G-ENDNLYNB9G');
 
 function App() {
   return (
