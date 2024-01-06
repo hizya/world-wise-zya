4a6692469354c080174de0722e131889662bcd7e
张宇昂 <zhangyuang@zhangyuangdeMacBook-Pro.local>
2024-01-06 17:06:39 +0800

    come back

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import ReactGA from 'react-ga4';
-ReactGA.initialize('G-ENDNLYNB9G');
-
-ReactGA.send({
-  hitType: 'pageView',
-  page: window.location.pathname,
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
